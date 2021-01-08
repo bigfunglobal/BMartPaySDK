5a28699bbbdb6697fef5e76ec02caf5c3225a0a0
lqq <495776706@qq.com>
2021-01-08 18:28:03 +0800

    新版本1.0.2

--- paytmlibrary/src/main/java/com/bigfun/tm/BigFunSDK.java
+++ paytmlibrary/src/main/java/com/bigfun/tm/BigFunSDK.java
@@ -32,7 +32,7 @@ public class BigFunSDK {
      */
     static boolean isDebug = false;
     private static String mSource;
-    private static final String VERSION = "1.0.1";
+    private static final String VERSION = "1.0.2";
 
     private BigFunSDK() {
 
